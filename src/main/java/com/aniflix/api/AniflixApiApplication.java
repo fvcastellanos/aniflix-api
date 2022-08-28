d0b413c5ef862bcfbbdeeec64561a79d6625a4c0
Francisco Castellanos <fvcastellanos@gmail.com>
2022-08-27 22:12:22 -0600

    add java project template

---
+++ src/main/java/com/aniflix/api/AniflixApiApplication.java
@@ -0,0 +1,13 @@
+package com.aniflix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniflixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AniflixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aniflix/api/AniflixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniflix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniflixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
